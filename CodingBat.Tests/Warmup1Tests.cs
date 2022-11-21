a8d8b4c2f4361e3ab783ed1b1126b66dcf2788f4
veyselalperen <veyselalperenceylan@gmail.com>
2022-11-21 16:14:24 +0300

    fix test

--- CodingBat.Tests/Warmup1Tests.cs
+++ CodingBat.Tests/Warmup1Tests.cs
@@ -31,7 +31,7 @@ namespace CodingBat.Tests
         [TestCase(1, 2, 3)]
         [TestCase(3, 2, 5)]
         [TestCase(2, 2, 8)]
-        [TestCase(-1, 2, -1)]
+        [TestCase(-1, 2, 1)]
         [TestCase(3, 3, 12)]
         [TestCase(0, 0, 0)]
         [TestCase(0, 1, 1)]
